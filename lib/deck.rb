045ed87a56afce35c3d8be4b7896046613ffe155
Cory Bretsch <corybretsch@gmail.com>
2024-05-10 16:43:27 -0500

    methods and tests for class Deck: init, rank_of_card_at, high_ranking_cards, percent_high_ranking)

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck 
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+  end
+
+  def high_ranking_cards
+    cards.select do |card|
+      card.rank > 9
+    end
+  end
+
+  def percent_high_ranking
+    ((high_ranking_cards.count/cards.count.to_f)*100).round(2)
+  end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,13 +1,91 @@
 require 'rspec'
 require './lib/card'
+require './lib/deck'
 
 RSpec.configure do |config|
   config.formatter = :documentation
 end
 
-RSpec.describe Card do
+RSpec.describe Deck do
   it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
+    expect(deck).to be_a Deck
   end
-  
+
+  it "has readable attributes" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq ([card1, card2, card3])
+  end
+
+  it "can return rank of card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to be 12
+    expect(deck.rank_of_card_at(2)).to be 14
+  end
+
+  it "can list high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq [card1, card3]
+  end
+
+  it "can give high card/deck ratio" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to be 66.67
+  end
 end
+
+
+
+
+
+
+# deck.remove_card
+# #=> #<Card:0x007fbfd18490e8 @rank=12, @suit=:diamond, @value="Queen">
+
+# deck.cards
+# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
+
+# deck.high_ranking_cards
+# #=> [#<Card:0x007fbfd18555a0...>]
+
+# deck.percent_high_ranking
+# #=> 50.0
+
+# card4 = Card.new(:club, '5', 5)
+# #=> #<Card:0x007fbfd2963978 @rank=5, @suit=:club, @value="5">
+
+# deck.add_card(card4)
+
+# deck.cards
+# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>, #<Card:0x007fbfd2963978...>]
+
+# deck.high_ranking_cards
+# #=> [#<Card:0x007fbfd18555a0...>]
+
+# deck.percent_high_ranking
+# #=> 33.33
